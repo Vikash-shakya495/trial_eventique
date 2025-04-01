6c758147e8ddf22cc6fb5af4cad8b25a5f2da8e3
Vikash Shakya <vikashshakya367@gmail.com>
2025-04-01 15:27:41 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
 })
 
-export const API_URL = "https://eventique-backend-api.onrender.com";
+
+export const API_URL = import.meta.env.VITE_API_URL;
